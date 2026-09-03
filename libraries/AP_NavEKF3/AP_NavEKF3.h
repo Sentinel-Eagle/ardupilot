b8617450df3e76ed888617d67c0c48799ed859e8
Michał Kucharczyk <33499847+quczer@users.noreply.github.com>
2026-09-03 12:46:32 +0200

    fix mission planner plugin (#31)

--- libraries/AP_NavEKF3/AP_NavEKF3.h
+++ libraries/AP_NavEKF3/AP_NavEKF3.h
@@ -298,7 +298,7 @@ public:
     // send an EKF_STATUS_REPORT message to GCS
     void send_status_report(class GCS_MAVLINK &link) const;
 
-    void send_eagle_lane_states_report(class GCS_MAVLINK &link) const;
+    void send_eagle_lanes_state_report(class GCS_MAVLINK &link) const;
 
     // provides the height limit to be observed by the control loops
     // returns false if no height limiting is required
