5f4b5b3eb4c1bd685cee0313aa650313cbd01303
Michał Kucharczyk <kucharczi@gmail.com>
2026-09-03 11:43:21 +0200

    claude vibe

--- libraries/AP_NavEKF3/AP_NavEKF3.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3.cpp
@@ -2074,13 +2074,13 @@ void NavEKF3::send_status_report(GCS_MAVLINK &link) const
     }
 }
 
-void NavEKF3::send_eagle_lane_states_report(GCS_MAVLINK &link) const
+void NavEKF3::send_eagle_lanes_state_report(GCS_MAVLINK &link) const
 {
     if (!core) {
         return;
     }
 
-    mavlink_eagle_lane_states_t packet{};
+    mavlink_eagle_lanes_state_t packet{};
     packet.active_lane = primary;
     packet.lane_count = num_cores;
 
@@ -2102,7 +2102,7 @@ void NavEKF3::send_eagle_lane_states_report(GCS_MAVLINK &link) const
         packet.posxy_source[i] = uint8_t(core[i].get_posxy_source());
     }
 
-    mavlink_msg_eagle_lane_states_send_struct(link.get_chan(), &packet);
+    mavlink_msg_eagle_lanes_state_send_struct(link.get_chan(), &packet);
 }
 
 // provides the height limit to be observed by the control loops
